eed00ee03601e8a779149dedb0c2a3baa6cea3f8
Nunyaklah <nunyaklah@gmail.com>
2021-01-28 19:06:59 +0000

    call by reference

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void swap (int *a, int *b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+void swap (int &a, int &b){
+  int temp = a;
+  a = b;
+  b = temp;
 
 }
 
