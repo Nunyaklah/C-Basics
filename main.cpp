54373f93880bd9da3c690825d1a511036ceab010
Nunyaklah <nunyaklah@gmail.com>
2021-01-28 15:47:32 +0000

    structure example

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+struct CarData{
+  char reg_no[8];
+  char model[30];
+  int year;
+  float mileage;
+};
 
 int main() {
- int arr[10]; //an array containing ten elements int
-
- char rev[] = "reverse";
- int i =0 ;
- while (rev[i] != '\0')
- i++;
- cout<<"No of chars: " <<i;
- while (i > 0)
- {
-   --i;
-   cout << rev[i];
- }
+ CarData mine, yours;
+ mine.year = 2013;
+ strcpy(mine.model, "Land Rover");
+ yours =mine;
 }
