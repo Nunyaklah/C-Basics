a4e917b1445adf6ad535d98b5e7d26010a21b6eb
Nunyaklah <nunyaklah@gmail.com>
2021-01-28 13:17:22 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  int total;
+  cout<< "enter total: ";
+  cin>>total;
+
+  int *t_ptr = &total;
+  cout<<total<< "\n"<< *t_ptr<<endl;
+
+  ++*t_ptr;
+  cout<< total <<"\n"<<*t_ptr;
+}
