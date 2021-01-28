6996f31d772062d047f5d894c59ff34300526bf9
Nunyaklah <nunyaklah@gmail.com>
2021-01-28 13:22:12 +0000

    Work on references

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 
 int main() {
-  int total;
-  cout<< "enter total: ";
-  cin>>total;
-
-  int *t_ptr = &total;
-  cout<<total<< "\n"<< *t_ptr<<endl;
-
-  ++*t_ptr;
-  cout<< total <<"\n"<<*t_ptr;
+ int i = 5;
+ int &ir = i;
+ cout << i << ir <<endl;
+ ++i;
+ cout << i << ir <<endl;
+ ++ir;
+ cout << i <<ir << endl;
 }
